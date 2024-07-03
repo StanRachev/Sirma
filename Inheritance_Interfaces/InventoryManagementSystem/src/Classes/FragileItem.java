6cedfb55acc87bcb2ccb127c3abc16f2f2511b9c
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-07-04 02:27:46 +0300

    updated the calculateValue method

--- Inheritance_Interfaces/InventoryManagementSystem/src/Classes/FragileItem.java
+++ Inheritance_Interfaces/InventoryManagementSystem/src/Classes/FragileItem.java
@@ -1,18 +1,26 @@
 package Classes;
 
 public class FragileItem extends InventoryItem {
-    private int weight;
+    private double weight;
     private String material;
 
-    public FragileItem(int weight, String material, String category, double price,
+    public FragileItem(double weight, String material, String category, double price,
                        int itemId, int quantity) {
         super(category, true, false, price, itemId, quantity);
         this.weight = weight;
         this.material = material;
     }
 
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
     @Override
     public double calculateValue() {
-        return getQuantity() * getItemPrice() * this.weight;
+        return getQuantity() * getItemPrice() + (this.weight / 1000);
     }
 }
