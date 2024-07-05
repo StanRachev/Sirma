b678b3c2c284153f7b92791ee30d2839a20aa39f
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-07-05 14:06:16 +0300

    changed the id to be autogenerated

--- Inheritance_Interfaces/InventoryManagementSystem/src/Classes/InventoryItem.java
+++ Inheritance_Interfaces/InventoryManagementSystem/src/Classes/InventoryItem.java
@@ -1,18 +1,14 @@
 package Classes;
 
 public class InventoryItem extends AbstractItem {
+    private static int idCntr = 0;
     private int itemId;
     private int quantity;
 
-    public InventoryItem(String category, boolean breakable, boolean perishable, double price,
-                         int itemId, int quantity) {
+    public InventoryItem(String category, boolean breakable, boolean perishable, double price, int quantity) {
         super(category, breakable, perishable, price);
-        this.itemId = itemId;
+        this.itemId = ++idCntr;
         this.quantity = quantity;
-    }
-
-    public InventoryItem() {
-
     }
 
     public int getItemId() {
