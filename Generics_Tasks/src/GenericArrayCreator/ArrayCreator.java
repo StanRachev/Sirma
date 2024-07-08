1c0b804b1248f580fbdc26ac1ea46a63cdab51f4
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-07-08 13:24:55 +0300

    Added the implementation

--- Generics_Tasks/src/GenericArrayCreator/ArrayCreator.java
+++ Generics_Tasks/src/GenericArrayCreator/ArrayCreator.java
@@ -1,13 +1,27 @@
 package GenericArrayCreator;
 
+import java.lang.reflect.Array;
+
 public class ArrayCreator<T> {
 
-
-/*    public static T[] create(int length, T item) {
-
+    public ArrayCreator() {
     }
 
-    public static T[] create(Class<T> classT, int length, T item) {
+    public static <T> T[] create(int length, T item) {
+        T[] items = (T[]) new Object[length];
 
-    }*/
+        for (int i = 0; i < items.length; i++) {
+            items[i] = item;
+        }
+        return items;
+    }
+
+    public static <T> T[] create(Class<T> classT, int length, T item) {
+        T[] items = (T[]) Array.newInstance(classT, length);
+
+        for (int i = 0; i < items.length; i++) {
+            items[i] = item;
+        }
+        return items;
+    }
 }
