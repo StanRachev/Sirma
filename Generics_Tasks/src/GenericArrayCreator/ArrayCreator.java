f06c93ab3d7488bc35b5e0047277b3616a68ece9
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-07-08 11:33:42 +0300

    class that returns an array of every type, initizlized to a default value

---
+++ Generics_Tasks/src/GenericArrayCreator/ArrayCreator.java
@@ -0,0 +1,13 @@
+package GenericArrayCreator;
+
+public class ArrayCreator<T> {
+
+
+/*    public static T[] create(int length, T item) {
+
+    }
+
+    public static T[] create(Class<T> classT, int length, T item) {
+
+    }*/
+}
