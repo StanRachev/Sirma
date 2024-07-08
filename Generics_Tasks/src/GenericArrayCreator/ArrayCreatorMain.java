a7e0afbf63725d33f2c079235e1a47f0ebdfb05c
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-07-08 11:34:20 +0300

    main class for the ArrayCreator

---
+++ Generics_Tasks/src/GenericArrayCreator/ArrayCreatorMain.java
@@ -0,0 +1,8 @@
+package GenericArrayCreator;
+
+public class ArrayCreatorMain {
+    // Class that creates an Array of a given Class
+    // Returns an Array with given length and default item
+
+    
+}
