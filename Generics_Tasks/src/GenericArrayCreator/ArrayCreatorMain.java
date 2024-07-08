e7a944dd3ab668e250bd833e0c1f79000492eba5
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-07-08 13:25:09 +0300

    Added the implementation

--- Generics_Tasks/src/GenericArrayCreator/ArrayCreatorMain.java
+++ Generics_Tasks/src/GenericArrayCreator/ArrayCreatorMain.java
@@ -1,8 +1,25 @@
 package GenericArrayCreator;
 
 public class ArrayCreatorMain {
-    // Class that creates an Array of a given Class
-    // Returns an Array with given length and default item
+    public static void main(String[] args) {
+        // Class that creates an Array of a given type
+        // Returns an Array with given length and default item
+        
+        String[] arrStr = ArrayCreator.create(String.class,5, "Haha");
+        print(arrStr);
 
-    
+        Integer[] arrInt = ArrayCreator.create(Integer.class,5, 1992);
+        print(arrInt);
+    }
+
+    private static <T> void print(T[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+    }
 }
