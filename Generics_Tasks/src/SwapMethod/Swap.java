cf559ac9b91b4472956d57caa3d33da2421d6f8a
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-07-09 11:24:30 +0300

    Class Swap that swaps two indices of a list

---
+++ Generics_Tasks/src/SwapMethod/Swap.java
@@ -0,0 +1,28 @@
+package SwapMethod;
+
+import java.util.List;
+
+public class Swap<T> {
+
+    public Swap() {
+    }
+
+    public void swap(List<T> list, int in1, int in2) {
+        if (in1 < 0 || in1 >= list.size() || in2 < 0 || in2 >= list.size()) {
+            throw new ArrayIndexOutOfBoundsException("Out of bounds array indices.");
+        }
+        T temp = list.get(in1);
+        list.set(in1, list.get(in2));
+        list.set(in2, temp);
+    }
+
+    public void print(List<T> list) {
+        for (var item : list) {
+            if (item != null) {
+                System.out.println(item.getClass().getName() + ": " + item);
+            } else {
+                System.out.println("Null item");
+            }
+        }
+    }
+}
