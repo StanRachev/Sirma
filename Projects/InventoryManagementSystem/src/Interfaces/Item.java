f4b858391c4fe7dbaa97bf065dc850e3321a00e8
Stanimir Rachev <rachev.stanimir7@gmail.com>
2024-07-21 22:45:05 +0300

    changed the return type of getItemDetails() from void to String

--- Projects/InventoryManagementSystem/src/Interfaces/Item.java
+++ Projects/InventoryManagementSystem/src/Interfaces/Item.java
@@ -1,7 +1,6 @@
 package Interfaces;
 
 public interface Item {
-    public void getItemDetails();
+    public String getItemDetails();
     public double calculateValue();
-    public String getItemsDescription();
 }
